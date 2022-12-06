e05940cb85f92d5a4d2651cdb484df168a57d47d
mariamsim202229 <mariamsimonyan1234@gmail.com>
2022-12-06 14:40:46 +0100

    new commit

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-//Kopplar HTML element till Javascript 
 
 inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value) //Lägger till text som skrivs i input fält i arrayen myLeads
     inputEl.value = "" //input fält blir tom efter knappen klickas
-    renderLeads() //Kör renderLeads funktionen
     const li = document.createElement("li");
 
 
-    li.textContent = "";
+    li.textContent = "exempel";
 
     const button = document.createElement("button");
     button.textContent = "Ta bort"
@@ -20,34 +16,20 @@ inputBtn.addEventListener("click", function () {
         e.target.parentElement.remove();
     });
 
+    const button2 = document.createElement("button");
+    button2.textContent = "Markera"
+    button2.addEventListener("click", function (e) {
+        console.log("Någon klickade...", e.target.parentElement);
+        e.target.parentElement.highlight();
+    });
+
 
     ulEl.appendChild(li);
     li.appendChild(button);
+    li.appendChild(button2);
 
 
 })
-
-
-function renderLeads() {
-    let listItems = "" //skapar en ny variabel som innehåller en tom sträng
-    //loopen körs lika många gånger som arrayen myLeads är lång
-    // Variabeln i har olika värden beroende på vilket varv loopen är på.
-    for (let i = 0; i < myLeads.length; i++) {
-        //  <li> element läggs till listItmes
-        listItems += `
-            <li>
-                    ${myLeads[i]}
-            
-            </li>
-        `
-        // Ett <li> element skapas för varje sträng i arrayen och läggs till som text
-
-
-        ulEl.innerHTML = listItems // Skickar in alla <li> element i HTML elementet i HTML filen
-    }
-}
-
-
 
 
 
